bea92a6cd373067d09ebd66dfaea939c8a496fb7
Tim Gentry <gentry.tim@gmail.com>
2020-09-04 13:04:26 -0400

    handling note not found with 404

--- index.js
+++ index.js
@@ -22,9 +22,13 @@ let notes = [
     }
 ];
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-});
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const note = notes.find(note => note.id === id);
+
+    note ? response.json(note) : response.status(404).end();
+})
 
 app.get('/api/notes', (req, res) => {
     res.json(notes)
